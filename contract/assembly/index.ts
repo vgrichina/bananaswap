305cb383e45285ee2945ee52c765dba99bb3f09d
Vladimir Grichina <vgrichina@gmail.com>
2020-12-02 22:38:12 -0800

    Make "sell" part kinda work

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -12,7 +12,7 @@
  *
  */
 
-import { context, ContractPromise, ContractPromiseBatch, logging, storage, u128, u128 } from 'near-sdk-as'
+import { context, ContractPromise, ContractPromiseBatch, logging, storage, u128 } from 'near-sdk-as'
 
 const BERRIES_CONTRACT = 'berryclub.ek.near';
 const NEAR_NOMINATION = u128.from('1000000000000000000000000');
@@ -27,7 +27,6 @@ export function start(): void {
     assert(storage.contains('berries'), 'must deposit berries before starting');
 
     const availableBalance = context.accountBalance - MIN_BALANCE;
-    storage.set('k', storage.get<u128>('berries') * availableBalance);
     storage.set('started', true);
 }
 
@@ -48,9 +47,9 @@ export function buy(berries: u128): ContractPromiseBatch {
     assert(berries < internalBerries, 'not enough berries in pool');
 
     const resultingBerries = internalBerries - berries;
-    const newNearAmount = storage.get<u128>('k') / resultingBerries;
-    // TODO: What to do with remainder?
     const currentNearAmount = context.accountBalance - MIN_BALANCE;
+    const newNearAmount =  internalBerries * currentNearAmount / resultingBerries;
+    // TODO: What to do with remainder?
     const nearPrice = newNearAmount - currentNearAmount;
     assert(nearPrice <= context.attachedDeposit, 'not enough NEAR attached, required ' + nearPrice.toString());
 
@@ -72,16 +71,14 @@ function sell(sender_id: string, berries: u128, nearAmount: u128): u128 {
 
     const newNear = currentNearAmount - nearAmount;
     const currentBerries = storage.getSome<u128>('berries');
-    const k = storage.getSome<u128>('k');
-    const newBerries = k / newNear;
+    const newBerries = currentBerries * currentNearAmount / newNear;
     // TODO: What to do with remainder?
     const berriesPrice = newBerries - currentBerries;
-    logging.log('k: ' + k.toString());
-    logging.log('currentNearAmount: ' + currentNearAmount.toString());
-    logging.log('newNear: ' + newNear.toString());
-    logging.log('currentBerries: ' + currentBerries.toString());
-    logging.log('newBerries: ' + newBerries.toString());
-    logging.log('hey: ' + (u128.from('9801427805542018869750100000000') / u128.from('10')).toString());
+    // logging.log('nearAmount: ' + nearAmount.toString());
+    // logging.log('currentNearAmount: ' + currentNearAmount.toString());
+    // logging.log('newNear: ' + newNear.toString());
+    // logging.log('currentBerries: ' + currentBerries.toString());
+    // logging.log('newBerries: ' + newBerries.toString());
     assert(berriesPrice <= berries, 'not enough berries attached, required ' + berriesPrice.toString());
 
     // TODO: Do we need to lock somehow before transfer end?
@@ -106,11 +103,11 @@ function withdrawFromVault(vault_id: string, receiver_id: string, amount: u128):
 }
 
 export function on_receive_with_vault(sender_id: string, amount: u128, vault_id: string, payload: String): ContractPromise {
-    // assert(context.predecessor == BERRIES_CONTRACT, "can only be called from token contract");
+    assert(context.predecessor == BERRIES_CONTRACT, "can only be called from token contract");
 
     if (payload.startsWith('sell:')) {
         const parts = payload.split(':');
-        const nearAmount = u128.from(parts[0]);
+        const nearAmount = u128.from(parts[1]);
         const berries = sell(sender_id, amount, nearAmount);
         if (berries < amount) {
             withdrawFromVault(vault_id, sender_id, amount - berries);
